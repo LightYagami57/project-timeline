293d0b996ab0e28969925d6b4a45d7b733aac4f5
LightYagami57 <javedkrim57@gmail.com>
2026-01-01 12:00:00 +0530

    Project update 2026-01-01 12:00:00

--- script.ps1
+++ script.ps1
@@ -1,21 +1,20 @@
 $start = Get-Date "2026-01-01"
-$end = Get-Date "2026-12-01"
+$end = Get-Date "2026-05-01"
 
 $current = $start
 
 while($current -le $end){
 
-Add-Content timeline.txt "Update $current"
+    Add-Content timeline.txt "Update $($current.ToString('yyyy-MM-dd'))"
 
-git add .
+    git add .
 
-$date=$current.ToString("yyyy-MM-dd HH:mm:ss")
+    $date = $current.ToString("yyyy-MM-dd 12:00:00")
 
-$env:GIT_AUTHOR_DATE=$date
-$env:GIT_COMMITTER_DATE=$date
+    $env:GIT_AUTHOR_DATE = $date
+    $env:GIT_COMMITTER_DATE = $date
 
-git commit -m "Project update $date"
+    git commit -m "Project update $date"
 
-$current=$current.AddDays(1)
-
+    $current = $current.AddDays(1)
 }
